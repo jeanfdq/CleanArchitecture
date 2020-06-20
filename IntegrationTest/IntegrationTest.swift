c00c78d14002f622cd9b47446fc1bef89ccdedf1
Jean Paull <jeanfdq@gmail.com>
2020-06-20 03:14:45 -0300

    add IntegrationTest with Failure

--- IntegrationTest/IntegrationTest.swift
+++ IntegrationTest/IntegrationTest.swift
@@ -35,7 +35,29 @@ class IntegrationTest: XCTestCase {
             exp.fulfill()
         }
 
-        wait(for: [exp], timeout: 2)
+        wait(for: [exp], timeout: 5)
+    }
+    
+    func test_AddAccount_with_failure() {
+        
+        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let alamofireAdapter = AlamofireAdapter()
+        let addAccountModel = AddAccountModel(name: "Jean Paull", email:"jean123@jean.com", password:"secret", passwordConfirmation: "secret2")
+        let sut = RemoteAddAccount(url: url, httpPostClient: alamofireAdapter)
+        let exp = expectation(description: "waiting")
+        sut.add(addAccountModel: addAccountModel) { result in
+            
+            switch result {
+                
+            case .failure (let error): XCTAssertEqual(error, .unexpected)
+            case .success: XCTFail("Expected only Error")
+                
+            }
+            
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 5)
     }
 
 
