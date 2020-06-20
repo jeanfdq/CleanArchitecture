c71fda21f0d2c8fdfa300754342be5aa4426ed32
Jean Paull <jeanfdq@gmail.com>
2020-06-19 22:29:47 -0300

    ensure Alamofire URL

--- InfraTests/InfraTests.swift
+++ InfraTests/InfraTests.swift
@@ -7,10 +7,28 @@
 //
 
 import XCTest
-@testable import Infra
+import Alamofire
 
-class InfraTests: XCTestCase {
 
+class AlamofireAdapter {
+    
+    private let session: Session
+
+    init( with session:Session = .default) {
+        self.session = session
+    }
+
+    func post(to url:URL) {
+        session.request(url).resume()
+    }
+    
+}
+
+
+class AlamofireAdapterTeste: XCTestCase {
+
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -20,15 +38,43 @@ class InfraTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        
+        let url = URL(string: "https://any_url.com.br")!
+        let config = URLSessionConfiguration.default
+        config.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: config)
+        let sut = AlamofireAdapter(with: session)
+        sut.post(to: url)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observerRequest { (request) in
+            XCTAssertEqual(url, request.url)
+            exp.fulfill()
         }
+        wait(for: [exp], timeout: 1)
     }
 
+}
+
+class UrlProtocolStub: URLProtocol {
+    
+    static var completion: ((URLRequest)->Void)?
+    
+    static func observerRequest(completion:@escaping(URLRequest)->Void){
+        UrlProtocolStub.completion = completion
+    }
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        UrlProtocolStub.completion?(request)
+    }
+    
+    override func stopLoading() {}
+    
 }
